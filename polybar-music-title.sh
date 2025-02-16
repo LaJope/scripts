292547f031f3eb7851beb4af841876d95f52176c
LaJope <Siephan2.0.0.3@gmail.com>
2025-02-16 20:01:51 +0300

    New

--- polybar-music-title.sh
+++ polybar-music-title.sh
@@ -5,7 +5,6 @@ response="$(playerctl --player="$player" status 2>&1)"
 
 case $response in
   'Playing') echo "$(playerctl --player="$player" metadata xesam:title) ($(playerctl --player="$player" metadata xesam:artist))" ;;
-  # 'Playing') playerctl --player="$player" metadata xesam:title ;;
   'Paused') echo '' ;;
   '*') echo '' ;;
 esac
