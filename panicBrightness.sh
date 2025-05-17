50f97d8849173788b2b19e078dfe1ef999e4071f
LaJope <Siephan2.0.0.3@gmail.com>
2025-05-17 23:54:47 +0300

    New

---
+++ panicBrightness.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+panicBrightness=30
+normalBrightness=7500
+
+curr=$(brightnessctl get)
+
+if [ $curr != $panicBrightness ] ; then
+    brightnessctl set $panicBrightness ;
+else
+    brightnessctl set $normalBrightness;
+fi
